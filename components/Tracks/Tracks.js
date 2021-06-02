04e3effe66a0f5dec2f7574356ee5872b8a6d7c2
yalcinaksakal <yalcin.aksakal.tr@gmail.com>
2021-06-03 02:50:54 +0300

    tracks

--- components/Tracks/Tracks.js
+++ components/Tracks/Tracks.js
@@ -1,25 +1,14 @@
+import { template } from "../../config/config";
 import styles from "./Tracks.module.css";
-const template = `Introduction
- About mentor
- About track
- Prequisites
-Milestone 1: Title
- Subtitle
- Subtitle
-Milestone 2: Title
- Subtitle
-  Subsubtitle
-...
 
-Paste your track's curriculum here like above.
-After each line there has to be a linebreak (Enter).
-Number of spaces at the begining of line defines indent of title.`;
 const Tracks = ({ content }) => {
   console.log(content);
   return (
     <section>
       {content}
-      <textarea className={styles.text} placeholder={template}></textarea>
+      {content === "mentoring" && (
+        <textarea className={styles.text} placeholder={template}></textarea>
+      )}
     </section>
   );
 };
